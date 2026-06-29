63295a5106661a6140922bb799695b32fc66c4a6
edwars-fuentes <edw.fuentes@duocuc.cl>
2026-06-29 15:33:01 -0400

    servicioDeEnviosMS
    se encarga de ver los envios de cada usuarios

---
+++ servicioDeEnviosMS-/servicioDeEnviosMS/src/main/java/cl/duoc/servicioDeEnviosMS/ServicioDeEnviosMsApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.servicioDeEnviosMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioDeEnviosMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioDeEnviosMsApplication.class, args);
+	}
+
+}
---
+++ servicioDeEnviosMS-/servicioDeEnviosMS/src/test/java/cl/duoc/servicioDeEnviosMS/ServicioDeEnviosMsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.servicioDeEnviosMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioDeEnviosMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
